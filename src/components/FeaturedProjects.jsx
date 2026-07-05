28970028307abddbcec3c35b01074323ae8a5b3a
MiyaAadil <miyaaadil76@gmail.com>
2026-07-05 22:39:19 +0545

    Featured projects section UI  was polished

--- src/components/FeaturedProjects.jsx
+++ src/components/FeaturedProjects.jsx
@@ -5,8 +5,9 @@ const FeaturedProjects = () => {
   return (
     <div className='flex flex-col gap-2 m-5 lg:max-w-7xl lg:mx-auto'>
       <h1 className='text-4xl text-slate-900 font-bold'>Featured Projects</h1>
+      <p className='text-gray-500 text-lg'>Explore some of our recent digital solutions that have helped businesses grow.</p>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
         {projects.map((project) => (
         <ProjectCard key={project.id} title={project.title} category={project.category} image={project.image} />
         ))}
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,22 +2,19 @@ import React from 'react'
 
 const ProjectCard = ({title, category, image}) => {
   return (
-    <div className='h-90 flex flex-col bg-gray-400 p-3 rounded-4xl cursor-pointer hover:translate-y-1 transition-all duration-300 shadow-xl relative overflow-hidden'>
+    <div className='h-90 flex flex-col bg-gray-400 p-3 rounded-4xl cursor-pointer hover:translate-y-1 transition-all duration-300 shadow-xl relative overflow-hidden group-hover:scale-105'>
 
     <img className='h-full object-cover rounded-4xl absolute inset-0 w-full p-2' src={image} alt="" />
 
-      <div className='bg-black/50 backdrop:blur-4xl p-3 w-full flex absolute bottom-0 right-0 flex-col gap-2 text-white'>
+      <div className='bg-black/50 backdrop-blur-xs p-3 w-full flex absolute bottom-0 right-0 flex-col gap-2 text-white'>
 
-        <h2 className='text-2xl font-medium leading-3'>{title}</h2>
+        <h2 className='text-2xl font-medium leading-5'>{title}</h2>
 
-        <h2 className='text-gray-200 text-sm'>{category}</h2>
+        <h2 className='text-gray-200 text-xs bg-red-500/90 py-1 px-3 w-35 rounded-full'>{category}</h2>
 
         <button className='bg-white hover:bg-slate-200 text-black py-3 rounded-full cursor-pointer font-medium'>View Project</button>
       </div>
 
-      
-
-      
     </div>
   )
 }
