6a6144d85fa80b80f47f9db9af84dfd35edff41b
MiyaAadil <miyaaadil76@gmail.com>
2026-07-08 21:32:57 +0545

    Made some changes on mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import { Link, NavLink } from 'react-router-dom'
 import { useState, useContext } from 'react'
 import Logo from '../assets/images/Logo.png'
-import { Menu, X, House, Info, CircleUserRound, Moon, Sun   } from 'lucide-react'
+import { Menu, X, House, Info, CircleUserRound, Moon, Sun  } from 'lucide-react'
 import { ThemeContext } from "../context/ThemeContext";
+import { navLinks } from '../data/navLinks';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const navLinkStyle = ({ isActive }) => `transition-all duration-50 ${isActive ? "border-b-2" : "hover:text-gray-600 dark:hover:text-slate-400"}`;
+
 
 const Navbar = () => {
 
@@ -20,13 +24,15 @@ const Navbar = () => {
         </Link>
 
         {/* Desktop links for navbar */}
-        <div className='hidden md:flex md:gap-10 text-gray-900 dark:text-white text-sm uppercase font-medium'>
-          <Link className='hover:text-gray-600' to='/about'>About</Link>
-          <Link className='hover:text-gray-600' to='/blog'>Blog</Link>
-          <Link className='hover:text-gray-600' to='/contact'>Contact</Link>
-          <Link className='hover:text-gray-600' to='/projects'>Projects</Link>
-          <Link className='hover:text-gray-600' to='/services'>Services</Link>
+        <div className='hidden md:flex gap-5 text-sm uppercase font-medium'>
+          {navLinks.map((link) => (
+            <NavLink key={link.path} to={link.path} className={navLinkStyle}>
+              {link.name}
+            </NavLink>
+          ))}
+        
         </div>
+
       
       <div className='flex gap-5'>
         <button
@@ -35,6 +41,7 @@ const Navbar = () => {
         >
           {darkMode ? <Sun size={22} /> : <Moon size={22} />}
         </button>
+
 
         {/* Call to Action Button for Desktop */}
         <button className='hidden dark:bg-slate-700 dark:hover:bg-slate-900 md:flex bg-gray-800 text-white hover:bg-gray-950 px-3 py-2.5 rounded-xl active:scale-90 transition-all duration-500 cursor-pointer text-sm'>Book Appointment</button>
@@ -68,55 +75,20 @@ const Navbar = () => {
                 }`}
               >
 
-                {/* Header */}
-
-                {/* <div className="flex justify-between items-center">
-
-                </div> */}
-
                 {/* Links */}
 
                 <div className="flex flex-col px-5 font-sans gap-5 mt-15">
 
-                  <Link
-                    to="/about"
-                    onClick={() => setMenuOpen(false)}
-                    className="border-gray-200 text-2xl hover:text-gray-500 transition"
-                  >
-                    About
-                  </Link>
-
-                  <Link
-                    to="/services"
-                    onClick={() => setMenuOpen(false)}
-                    className="border-gray-200 text-2xl hover:text-gray-500 transition"
-                  >
-                    Services
-                  </Link>
-
-                  <Link
-                    to="/projects"
-                    onClick={() => setMenuOpen(false)}
-                    className="border-gray-200 text-2xl hover:text-gray-500 transition"
-                  >
-                    Projects
-                  </Link>
-
-                  <Link
-                    to="/blog"
-                    onClick={() => setMenuOpen(false)}
-                    className="border-gray-200 text-2xl hover:text-gray-500 transition"
-                  >
-                    Blog
-                  </Link>
-
-                  <Link
-                    to="/contact"
-                    onClick={() => setMenuOpen(false)}
-                    className="border-gray-200 text-2xl hover:text-gray-500 transition"
-                  >
-                    Contact
-                  </Link>
+                    {navLinks.map((link) => (
+                      <NavLink
+                        key={link.path}
+                        to={link.path}
+                        className={({isActive}) => isActive ? "border-b-3 font-semibold text-2xl" : "hover:text-gray-600 dark:hover:text-gray-500 transition-colors text-2xl"}
+                        onClick={() => setMenuOpen(false)}
+                      >
+                        {link.name}
+                      </NavLink>
+                    ))}
 
                 </div>
 
